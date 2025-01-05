88afddd86a518905278d6051eab675e26b991bc4
Sina <sinabm3@gmail.com>
2025-01-05 20:04:42 +0100

    Delete wrong url-mapping

--- src/main/java/com/sina/demo/backend/CompassGameEndpoint.java
+++ src/main/java/com/sina/demo/backend/CompassGameEndpoint.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/api/compassGame")
+@RequestMapping("/compassGame")
 public class CompassGameEndpoint {
 
     private final CompassGameService compassGameService;
