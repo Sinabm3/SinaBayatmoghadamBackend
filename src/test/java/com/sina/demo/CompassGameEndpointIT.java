6842b426090f4e214bfc81a2503bd9e88441e9eb
Sina <sinabm3@gmail.com>
2025-03-05 16:42:15 +0100

    Add Tests

--- src/test/java/com/sina/demo/CompassGameEndpointIT.java
+++ src/test/java/com/sina/demo/CompassGameEndpointIT.java
@@ -1,5 +1,8 @@
 package com.sina.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sina.demo.endpoint.dto.CompassGameDto;
+import com.sina.demo.endpoint.dto.QuestionDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +10,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import java.util.HashSet;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,9 +22,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class CompassGameEndpointIT {
 	private final MockMvc mockMvc;
+	private final ObjectMapper objectMapper;
 	@Autowired
-	CompassGameEndpointIT(MockMvc mockMvc) {
+	CompassGameEndpointIT(MockMvc mockMvc, ObjectMapper objectMapper) {
 		this.mockMvc = mockMvc;
+		this.objectMapper = objectMapper;
 	}
 
 	@Test
@@ -36,6 +42,26 @@ class CompassGameEndpointIT {
 						"\"verticalAxisPositiveName\":\"Up\",\"verticalAxisNegativeName\":\"Down\",\"password\":\"123456\"," +
 						"\"questionDtos\":[{\"text\":\"Test Question\",\"isHorizontal\":true,\"axisPower\":1}]}")))
 				.andExpect(status().isCreated()).andReturn().getResponse().getContentAsString();
+		mockMvc.perform(delete("/compassGame/"+id+"?password=123456")).andExpect(status().isOk());
+	}
+
+	@Test
+	void editAndDeleteTest() throws Exception {
+		String id = mockMvc.perform((post("/compassGame").contentType("application/json").content(
+				"{\"name\":\"Test Game\",\"horizontalAxisPositiveName\":\"Right\",\"horizontalAxisNegativeName\":\"Left\"," +
+						"\"verticalAxisPositiveName\":\"Up\",\"verticalAxisNegativeName\":\"Down\",\"password\":\"123456\"," +
+						"\"questionDtos\":[{\"text\":\"Test Question\",\"isHorizontal\":true,\"axisPower\":1}]}")))
+				.andExpect(status().isCreated()).andReturn().getResponse().getContentAsString();
+
+		QuestionDto questionDto = new QuestionDto(null, "bruh", true, 1);
+		HashSet<QuestionDto> questionDtos = new HashSet<>();
+		questionDtos.add(questionDto);
+		mockMvc.perform(put("/compassGame"+"?password=123456").contentType("application/json").
+						content(objectMapper.writeValueAsString(new CompassGameDto(Long.parseLong(id),"Test Game",
+								"Right","Left",
+								"Up","Down","123456",
+								questionDtos))))
+				.andExpect(status().isOk());
 		mockMvc.perform(delete("/compassGame/"+id+"?password=123456")).andExpect(status().isOk());
 	}
 
